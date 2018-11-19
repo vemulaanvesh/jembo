d780dd950dfc0adfe10e730d7250573dd062c249
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2018-11-19 14:00:40 +0530

    Update App.vue

--- VueDeployTest/src/App.vue
+++ VueDeployTest/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Bitfumes"/>
+    <HelloWorld msg="Welcome to PAYMATRIX"/>
   </div>
 </template>
 
