1a89922504c912015550dcd65515226c304db276
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2018-11-28 20:53:33 +0530

    Update vue.config.js

--- VueDeployTest/vue.config.js
+++ VueDeployTest/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     open: process.platform === 'darwin',
     host: '0.0.0.0',
-    port: 8080, // CHANGE YOUR PORT HERE!
+    port: 8000, // CHANGE YOUR PORT HERE!
     https: false,
     hotOnly: false,
   },
